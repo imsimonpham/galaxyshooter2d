3c5de2334588fe813d32b1b804b1a04a4001a843
simon <simonpham1294@gmai.com>
2023-06-23 16:38:55 -0300

    Added laser behaviour

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
+    [SerializeField] private float _speed = 6f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,10 @@ public class Laser : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+        if (transform.position.y > 6f)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,10 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float _speed = 4f;
     private float inputHorizontal;
-    
+    private float inputVertical;
+    [SerializeField] private GameObject _laserPrefab;
+    [SerializeField] private float _fireRate = 0.15f;
+    private float _canFire = -1f;
     
     // Start is called before the first frame update
     void Start()
@@ -18,9 +22,35 @@ public class Player : MonoBehaviour
     void Update()
     {
         CalculateMovement();
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            _canFire = Time.time + _fireRate;
+            Fire();
+        }
     }
 
     void CalculateMovement()
     {
+        inputHorizontal = Input.GetAxis("Horizontal");
+        inputVertical = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(inputHorizontal, inputVertical, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+        
+        //limit movement when exceeding the top and bottom of screen
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y,-5f, 5f) ,0);
+        
+        //player appears on the other side when exceeding the left and right of the scren
+        if (transform.position.x > 9.3f)
+        {
+            transform.position = new Vector3(-9.3f, transform.position.y, 0);
+        } else if (transform.position.x < -9.3f)
+        {
+            transform.position = new Vector3(9.3f, transform.position.y, 0); 
+        }
+    }
+
+    void Fire()
+    {
+        Instantiate(_laserPrefab, new Vector3(transform.position.x, transform.position.y + 0.8f, 0), Quaternion.identity);
     }
 }
